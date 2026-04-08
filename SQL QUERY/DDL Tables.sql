7e80fe73354a9f697548cc58130784cf6eb27665
Christian-Taurchini <chritau.projects@gmail.com>
2026-04-08 14:23:15 +0000

    Bronze configuration
    Creati file JSON per fase di ingestion

---
+++ SQL QUERY/DDL Tables.sql
@@ -0,0 +1,27 @@
+-- Bronze launches senza location
+CREATE TABLE IF NOT EXISTS esercizio_spacex.bronze.bronze_launches (
+    ingestion_timestamp TIMESTAMP,
+    raw_payload STRING
+)
+USING DELTA;
+
+-- Bronze rockets
+CREATE TABLE IF NOT EXISTS esercizio_spacex.bronze.bronze_rockets (
+    ingestion_timestamp TIMESTAMP,
+    raw_payload STRING
+)
+USING DELTA;
+
+-- Bronze launchpads
+CREATE TABLE IF NOT EXISTS esercizio_spacex.bronze.bronze_launchpads (
+    ingestion_timestamp TIMESTAMP,
+    raw_payload STRING
+)
+USING DELTA;
+
+-- Bronze payloads
+CREATE TABLE IF NOT EXISTS esercizio_spacex.bronze.bronze_payloads (
+    ingestion_timestamp TIMESTAMP,
+    raw_payload STRING
+)
+USING DELTA;
