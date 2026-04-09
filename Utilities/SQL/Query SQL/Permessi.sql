5b80cab0c145c6b98079c16d2755d97ec5c78f32
Christian-Taurchini <chritau.projects@gmail.com>
2026-04-09 13:34:53 +0000

    Query di permesso

---
+++ Utilities/SQL/Query SQL/Permessi.sql
@@ -0,0 +1,8 @@
+GRANT USE CATALOG ON CATALOG esercizio_spacex 
+TO `giordanocardone.lavoro@gmail.com`;
+
+GRANT USE SCHEMA ON SCHEMA esercizio_spacex.bronze 
+TO `giordanocardone.lavoro@gmail.com`;
+
+GRANT SELECT ON SCHEMA esercizio_spacex.bronze
+TO `giordanocardone.lavoro@gmail.com`;
